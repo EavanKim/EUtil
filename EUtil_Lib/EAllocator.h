7136e0e0e170950227c7a5765eebe80a3474acdd
eavankim <kjtkjt21@gmail.com>
2022-05-11 07:45:31 +0900

    [이슈 키워드] 메모리 관리 변경
    Ptr 동작 수정
    Allocator Ptr에서 쓰도록 등록
    
    향후 작업
    Allocator를 Singleton화 시키고 별도의 지역성을 가진 Allocator 생성

--- EUtil_Lib/EAllocator.h
+++ EUtil_Lib/EAllocator.h
@@ -13,7 +13,7 @@ namespace Eavan
 	*/
 
 	template<typename T>
-	class E_F_DLL EAllocator
+	class EAllocator
 	{
 	public:
 		explicit EAllocator();
@@ -21,9 +21,9 @@ namespace Eavan
 
 		virtual ~EAllocator();
 
-		T* allocate(size_t _size, const T* _ptr = nullptr);
-		void reallocate(size_t _size, const T* _ptr);
-		void delocate(T* _ptr, size_t);
+		T* allocate(const T*& _ptr = nullptr);
+		void reallocate(size_t _count, const T*& _ptr);
+		void delocate(T* _ptr, size_t _count = 1);
 
 		void CheckThreadVaildation(EBOOL& _IsOK);
 		void CheckMemoryVaildation(T* _ptr, EBOOL& _IsOK);
@@ -34,5 +34,77 @@ namespace Eavan
 		EVINT64 m_threadID = 0;
 		HANDLE m_heap = INVALID_HANDLE_VALUE;
 	};
+
+	template<typename T>
+	EAllocator<T>::EAllocator()
+		: m_heap(INVALID_HANDLE_VALUE)
+		, m_memorySize(0)
+		, m_allocCount(0)
+		, m_threadID(GetCurrentThreadId())
+	{
+		m_heap = HeapCreate(0, sizeof(T) * 10, sizeof(T) * 1024);
+		if (m_heap == INVALID_HANDLE_VALUE || m_heap == NULL)
+		{
+			throw EException();
+		}
+	}
+
+	template<typename T>
+	EAllocator<T>::EAllocator(const EAllocator<T>& _allocator)
+		: m_heap(INVALID_HANDLE_VALUE)
+		, m_memorySize(0)
+		, m_allocCount(0)
+		, m_threadID(GetCurrentThreadId())
+	{
+		if (m_heap != INVALID_HANDLE_VALUE)
+		{
+			HeapDestroy(m_heap);
+		}
+		m_heap = _allocator.m_heap;
+	}
+
+	template<typename T>
+	EAllocator<T>::~EAllocator()
+	{
+		HeapDestroy(m_heap);
+	}
+
+	template<typename T>
+	inline T* EAllocator<T>::allocate(const T*& _ptr)
+	{
+		_ptr = static_cast<T*>(HeapAlloc(m_heap, 0, sizeof(T)));
+		return _ptr;
+	}
+
+	template<typename T>
+	void EAllocator<T>::reallocate(size_t _count, const T*& _ptr)
+	{
+		_ptr = HeapReAlloc(m_heap, 0, _ptr, _count * sizeof(T));
+	}
+
+	template<typename T>
+	inline void EAllocator<T>::delocate(T* _ptr, size_t _count)
+	{
+		HeapFree(m_heap, 0, _ptr);
+	}
+
+	template<typename T>
+	void EAllocator<T>::CheckThreadVaildation(EBOOL& _IsOK)
+	{
+		DWORD GetThreadId = GetCurrentThreadId();
+		if (m_threadID == GetThreadId)
+			InterlockedExchange(&_IsOK, 1);
+		else
+			InterlockedExchange(&_IsOK, 0);
+	}
+
+	template<typename T>
+	void EAllocator<T>::CheckMemoryVaildation(T* _ptr, EBOOL& _IsOK)
+	{
+		if (HeapValidate(m_heap, 0, _ptr))
+			InterlockedExchange(&_IsOK, 1);
+		else
+			InterlockedExchange(&_IsOK, 0);
+	}
 }
 
